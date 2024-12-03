73a3f9584e25e9262acdc932b74b905f50a5ba62
Marta <153310274+martucs@users.noreply.github.com>
2024-12-03 16:22:30 +0100

    final version

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: martalop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 18:00:29 by martalop          #+#    #+#             */
-/*   Updated: 2024/12/02 21:48:13 by martalop         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:21:04 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "libft/libft.h"
+
+#ifndef INPUT_LINES
+#define INPUT_LINES 1000
+#endif
 
 int	fill_stacks(int *stack_a, int *stack_b)
 {
@@ -29,11 +33,11 @@ int	fill_stacks(int *stack_a, int *stack_b)
 	s_i = 0;
 	line = NULL;
 	line_arr = NULL;
-	fd = open("input2", O_RDWR);
+	fd = open("input", O_RDWR);
 	if (fd == -1)
 		return (write(2, "err\n", 4), 1);
 	line = get_next_line(fd);
-	while (line && s_i < 6)
+	while (line && s_i < INPUT_LINES)
 	{
 		line_arr = ft_split(line, ' ');
 		if (!line_arr)
@@ -48,28 +52,29 @@ int	fill_stacks(int *stack_a, int *stack_b)
 	return (0);
 }
 
-void	print_stack(int );
+void	print_stack(int *stack, int len)
+{
+	int	i;
 
-int	find_smallest(int *stack, /*int lenght,*/ int smallest)
+	i = 0;
+	while (stack && i < len)
+	{
+		printf("Stack[%d]: %d\n", i, stack[i]);
+		i++;
+	}
+}
+
+int	find_smallest(int *stack)
 {
 	int	min;
-	int	nb;
-	int	tmp;
 	int	i;
 	int	j;
 
-//	i = 0;
-//	tmp = -1;
 	min = 2147483647;
-//	if (smallest == -1)
-//		nb = -1;
-//	else
-//		nb = smallest;
 	i = 0;
 	j = 0;
-	while (i < 6)
+	while (i < INPUT_LINES)
 	{
-		//printf("Stack[%d]: %d\n", i, stack[i]);
 		if (stack[i] < min && stack[i] > -1)
 		{
 			min = stack[i];
@@ -78,27 +83,6 @@ int	find_smallest(int *stack, /*int lenght,*/ int smallest)
 		++i;
 	}
 	stack[j] = -1;
-	return (min);
-
-	while (stack && stack[i] && i < 6)
-	{
-		printf("stack = %d\n", stack[i]);
-		if (stack[i] == 0 && smallest == -1)
-		{
-			return (stack[i]);
-		}
-		if (stack[i] < min && stack[i] > -1)
-		{
-			min = stack[i];
-		}
-	/*	if (stack[i] == smallest && min == 2147483647)
-		{
-			printf("stack = %d\n", stack[i]);
-			printf("min = %d\n", min);
-			return (smallest);
-		}*/
-		i++;
-	}
 	return (min);
 }
 
@@ -112,59 +96,43 @@ int	*get_ordered_stack(int *stack)
 	i = 0;
 	j = 0;
 	smallest = -1;
-	new_stack = malloc(sizeof(int) * 6);
+	new_stack = malloc(sizeof(int) * INPUT_LINES);
 	if (!new_stack)
 		return (NULL);
-	while (i < 6)
+	while (i < INPUT_LINES)
 	{
-		//find_smallest
-		smallest = find_smallest(stack, smallest);
-	//	printf("stack in %d: %d\n", i, stack[i]);
+		smallest = find_smallest(stack); 
 		new_stack[i] = smallest;
-		printf("smallest = %d\n", smallest);
-	//	printf("new_stack in %d: %d\n", i, new_stack[i]);
-		//put it at the top after the last smallest number
 		i++;
 	}
 	return (new_stack);
 }
-
-/*int	main(void)
-{
-	int		*stack_a;
-	int		*stack_b;
-
-	stack_a = malloc(sizeof(int) * 1000);
-	if (!stack_a)
-		return (1);
-	stack_b = malloc(sizeof(int) * 1000);
-	if (!stack_b)
-		return (1);
-	fill_stacks(stack_a, stack_b);
-	//ordenar cada stack de menor a mayor
-	order_stacks(stack_a, stack_b);
-	// hacer restas de cada stack en la posicion en la que estan y guardarlas en algun sitio
-	// pasar los resultados a valores sin negativos
-	// sumar los resultados
-	return (0);
-}*/
+// *
+// smallest = find_smallest(stack);
+// new_stack[i] = smallest;
+//
+// we find the smallest number in 'stack' and we CHANGE its value to -1
+// we store the number found in 'new_stack'
+// 'stack' ends the while loop full of -1's
+// *
 
 int	*get_diff(int *stack_a, int *stack_b)
 {
-	int	*diff1;
+	int	*diff;
 	int	i;
 
-	i = 0:	
-	diff1 = malloc(sizeof(int) * 6);
-	if (!diff1)
+	i = 0;	
+	diff = malloc(sizeof(int) * 6);
+	if (!diff)
 		return (NULL);
-	while (i < 6)
+	while (i < INPUT_LINES)
 	{
 		diff[i] = stack_a[i] - stack_b[i];
 		if (diff[i] < 0)
-			diff[i] x= -1;
+			diff[i] *= -1;
 		i++;
 	}
+	return (diff);
 }
 
 int	do_diff_sum(int *array)
@@ -174,7 +142,7 @@ int	do_diff_sum(int *array)
 
 	i = 0;
 	res = 0;
-	while (i < 6)
+	while (i < INPUT_LINES)
 	{
 		res = res + array[i];
 		i++;
@@ -189,19 +157,30 @@ int	main(void)
 	int		*stack_b;
 	int		*ord_stack_a;
 	int		*ord_stack_b;
-	int		*diff1;
+	int		*diff;
+	int		sum_res;
 
-	stack_a = malloc(sizeof(int) * 6);
+	stack_a = malloc(sizeof(int) * INPUT_LINES);
 	if (!stack_a)
 		return (1);
-	stack_b = malloc(sizeof(int) * 6);
+	stack_b = malloc(sizeof(int) * INPUT_LINES);
 	if (!stack_b)
 		return (1);
 	fill_stacks(stack_a, stack_b);
+//	printf("before order\n");
+//	print_stack(stack_a, INPUT_LINES);
+//	printf("\n");
+//	print_stack(stack_b, INPUT_LINES);
 	ord_stack_a = get_ordered_stack(stack_a);
 	ord_stack_b = get_ordered_stack(stack_b);
-	diff1 = get_diff(stack_a, stack_b);
-	sum_res = do_diff_sum(diff1);
+//	printf("\nafter order\n");
+//	print_stack(ord_stack_a, INPUT_LINES);
+//	printf("\n");
+//	print_stack(ord_stack_b, INPUT_LINES);
+	diff = get_diff(ord_stack_a, ord_stack_b);
+//	printf("\ndiff\n");
+//	print_stack(diff, INPUT_LINES);
+	sum_res = do_diff_sum(diff);
 	printf("res = %d\n", sum_res);
 	return (0);
 }
