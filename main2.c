c6526695e28ff2201240f3664ca51068911491a9
Marta <153310274+martucs@users.noreply.github.com>
2024-12-04 04:10:11 +0100

    Update main2.c

--- main2.c
+++ main2.c
@@ -63,11 +63,51 @@ void    print_matrix(int **matrix, int length, int width)
     }
 }
 
+int safety_check(int first, int second, int *dir)
+{
+    if (!(*dir) && first < second)
+        *dir = 1;
+    else if (!(*dir) &&first > second)
+        *dir = -1;
+    diff = first - second;
+    diff = abs(diff);
+    if (diff > 3 || diff == 0)
+        return (0);
+    if (*diff)
+    {
+        if ((*diff == 1 && first > second) || (*diff == -1 first < second))
+            return (0);
+    }
+    return (1);
+}
+
 int main()
 {
     int **matrix;
+    int i;
+    int j;
+    int tmp;
+    int res;
+    int dir;
     
+    i = 0;
+    tmp = 0;
+    res = 0;
     matrix = get_data();
     print_matrix(matrix, 1000, 5);
-    return 0;
+    while (matrix && i < 1000)
+    {
+        j = 0;
+        dir = 0;
+        while (j < 5)
+        {
+            tmp = safety_check(matrix[i][j], matrix[i][j + 1], &dir);
+            res = res + tmp;
+            j++;
+        }
+        i++;
+    }
+	printf("res = %d/n", res);
+    return (res);
 }
+
